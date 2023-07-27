8b1f06894e46886c725612aed085be08a6c3b2e0
aali <abdelrahman.eia@gmail.com>
2023-07-28 00:37:50 +0300

    added base files for module

---
+++ accomodations/src/main/kotlin/hotel/accomodation/AccomodationsApplication.kt
@@ -0,0 +1,11 @@
+package hotel.accomodation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AccomodationsApplication
+
+fun main(args: Array<String>) {
+	runApplication<AccomodationsApplication>(*args)
+}
---
+++ accomodations/src/test/kotlin/hotel/accomodation/AccomodationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package hotel.accomodation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AccomodationsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
